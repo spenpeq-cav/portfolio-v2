83f29c035cec53ed355ee3c4e0e8820bec23fcba
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-22 18:28:17 -0700

    Footer contact form styling

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -4,8 +4,8 @@ function FooterSection() {
   return (
     <footer className="bg-gradient-to-tl from-neutral-800 to-neutral-900 py-12">
       <div className="px-4 py-8 max-w-xl mx-auto">
-        <div className="items-center border-4 border-red-500 rounded-md py-6 w-full bg-neutral-600 md:text-center">
-          <h1 className="text-center font-semibold text-neutral-200 text-5xl mb-6">
+        <div className="items-center border-4 border-blue-600 rounded-md py-6 w-full bg-neutral-800 md:text-center">
+          <h1 className="text-center font-bold text-neutral-200 text-5xl mb-6 tracking-wide">
             Contact Me
           </h1>
           <form
@@ -29,7 +29,7 @@ function FooterSection() {
               </div>
               <div className="md:w-3/4">
                 <input
-                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                   id="email"
                   type="email"
                   name="email"
@@ -49,7 +49,7 @@ function FooterSection() {
               </div>
               <div className="md:w-3/4">
                 <input
-                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                   id="name"
                   type="text"
                   name="name"
@@ -69,7 +69,7 @@ function FooterSection() {
               </div>
               <div className="md:w-3/4">
                 <input
-                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                   id="subject"
                   type="text"
                   name="subject"
@@ -88,7 +88,7 @@ function FooterSection() {
               </div>
               <div className="md:w-3/4">
                 <input
-                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+                  className="bg-neutral-200 appearance-none border-2 border-neutral-200 rounded w-full py-2 px-2 text-neutral-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                   id="message"
                   type="text"
                   name="message"
@@ -100,7 +100,7 @@ function FooterSection() {
             <div className="md:flex md:items-center">
               <div className="md:w-full">
                 <button
-                  className="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  className="shadow bg-blue-700 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-neutral-100 font-bold py-2 px-4 rounded"
                   type="submit"
                 >
                   Send Email
@@ -111,10 +111,10 @@ function FooterSection() {
         </div>
 
         <div className="pt-10">
-          <ul className="list-none flex text-center px-10">
+          <ul className="list-none flex text-center px-20">
             <li className="social-btn">
               <a href="https://github.com/spenpeq" target="_blank">
-                <i className="fab fa-github-square fa-3x"></i>
+                <i className="fab fa-github-square fa-4x"></i>
               </a>
             </li>
             <li className="social-btn">
@@ -122,7 +122,7 @@ function FooterSection() {
                 href="https://www.linkedin.com/in/spencer-pequegnat/"
                 target="_blank"
               >
-                <i className="fab fa-linkedin fa-3x"></i>
+                <i className="fab fa-linkedin fa-4x"></i>
               </a>
             </li>
             <li className="social-btn">
@@ -131,12 +131,12 @@ function FooterSection() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <i className="fab fa-twitter-square fa-3x"></i>
+                <i className="fab fa-twitter-square fa-4x"></i>
               </a>
             </li>
             <li className="social-btn">
               <a href="mailto: spencer.peq@gmail.com">
-                <i className="fas fa-envelope-square fa-3x"></i>
+                <i className="fas fa-envelope-square fa-4x"></i>
               </a>
             </li>
           </ul>
