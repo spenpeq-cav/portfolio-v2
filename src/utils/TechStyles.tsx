389707d075641cecc907eceb175114a0d205618d
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-27 22:20:49 -0700

    Created utils folder, TechStyles

---
+++ src/utils/TechStyles.tsx
@@ -0,0 +1,140 @@
+export const TechStyles = {
+  React: {
+    text: "React",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  Next: {
+    text: "Next.js",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  NextAuth: {
+    text: "Next-Auth",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  Typescript: {
+    text: "Typescript",
+    color: "text-blue-400 ",
+    icon1: "fa-solid fa-t fa-xs ",
+    icon2: "fa-solid fa-s fa-xs ",
+  },
+  Tailwind: {
+    text: "TailwindCSS",
+    color: "text-blue-300 ",
+    icon: "fas fa-wind ",
+  },
+  Postgres: {
+    text: "PostgreSQL",
+    color: "text-green-400 ",
+    icon: "fas fa-wind ",
+  },
+  Plaid: {
+    text: "Plaid API",
+    color: "text-gray-200 ",
+    icon: "fas fa-cogs ",
+  },
+  Prisma: {
+    text: "Prisma",
+    color: "text-gray-200 ",
+    icon: "fas fa-cogs ",
+  },
+  ReactRouter: {
+    text: "React Router",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  ReactContext: {
+    text: "Context API",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  JavaScript: {
+    text: "JavaScript",
+    color: "text-yellow-300 ",
+    icon: "fab fa-js-square ",
+  },
+  Node: {
+    text: "Node",
+    color: "text-green-500 ",
+    icon: "fab fa-node ",
+  },
+  Express: {
+    text: "Express",
+    color: "text-green-500 ",
+    icon: "fab fa-node ",
+  },
+  HTML: {
+    text: "HTML",
+    color: "text-yellow-600 ",
+    icon: "fab fa-html5 ",
+  },
+  Mongo: {
+    text: "MongoDB",
+    color: "text-green-400 ",
+    icon: "fas fa-database ",
+  },
+  Mongoose: {
+    text: "Mongoose",
+    color: "text-green-400 ",
+    icon: "fas fa-database ",
+  },
+  NPS: {
+    text: "National Park Service API",
+    color: "text-neutral-200 ",
+    icon: "fas fa-cogs ",
+  },
+  GoogleMaps: {
+    text: "Google Maps API",
+    color1: "text-neutral-200 ",
+    color2: "text-blue-500",
+    icon1: "fas fa-cogs ",
+    icon2: "fab fa-google ",
+  },
+  Angular: {
+    text: "Angular",
+    color: "text-red-600 ",
+    icon: "fab fa-angular ",
+  },
+  GitHub: {
+    text: "GitHub API",
+    color: "text-neutral-200 ",
+    icon: "fas fa-cogs ",
+  },
+  Redux: {
+    text: "Redux",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  Bootstrap: {
+    text: "React Bootstrap",
+    color: "text-blue-400 ",
+    icon: "fab fa-react ",
+  },
+  Python: {
+    text: "Python",
+    color: "text-yellow-300 ",
+    icon: "fab fa-python ",
+  },
+  Django: {
+    text: "Django",
+    color: "text-yellow-300 ",
+    icon: "fab fa-python ",
+  },
+  AWS_S3: {
+    text: "AWS S3",
+    color: "text-neutral-200 ",
+    icon: "fab fa-aws ",
+  },
+  PayPal: {
+    text: "PayPal API",
+    color: "text-neutral-200 ",
+    icon: "fas fa-cogs ",
+  },
+  JSON_Web_Tokens: {
+    text: "JSON Web Tokens",
+    color: "text-neutral-200 ",
+    icon: "fas fa-cogs ",
+  },
+};
