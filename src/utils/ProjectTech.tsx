52cb89b6eb6dffc852630fe227315266e08b5e7e
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-28 21:01:07 -0700

    ProjectTech utils

---
+++ src/utils/ProjectTech.tsx
@@ -0,0 +1,46 @@
+import { TechStyles as ts } from "./TechStyles";
+
+export const FinanceProjectTech = [
+  ts.React,
+  ts.Next,
+  ts.NextAuth,
+  ts.Tailwind,
+  ts.Tailwind,
+  ts.Postgres,
+  ts.Plaid,
+  ts.Prisma,
+];
+export const NationalParksProjectTech = [
+  ts.React,
+  ts.ReactRouter,
+  ts.ReactContext,
+  ts.JavaScript,
+  ts.Node,
+  ts.Node,
+  ts.Express,
+  ts.HTML,
+  ts.Tailwind,
+  ts.Mongo,
+  ts.Mongoose,
+  ts.NPS,
+  ts.GoogleMaps,
+];
+export const GitProfileProjectTech = [
+  ts.Angular,
+  ts.Typescript,
+  ts.JavaScript,
+  ts.HTML,
+  ts.GitHub,
+];
+export const EcommerceShopProjectTech = [
+  ts.React,
+  ts.Redux,
+  ts.Bootstrap,
+  ts.Python,
+  ts.Django,
+  ts.HTML,
+  ts.Postgres,
+  ts.AWS_S3,
+  ts.PayPal,
+  ts.JSON_Web_Tokens,
+];
