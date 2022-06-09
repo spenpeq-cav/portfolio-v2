354daa6283af898ae673741fdeae2df21c91be53
Spencer Pequegnat <spencer.peq@gmail.com>
2022-06-08 21:02:29 -0700

    Adding some top background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "top-image": "url('https://www.researchgate.net/publication/329220318/figure/fig2/AS:697638865338375@1543341475684/Trajectories-in-a-Cloud-Chamber-the-core-evidence-for-the-local-particle-nature-of.png')",
+        "top-image-2": "url('https://i.pinimg.com/736x/f3/b5/5c/f3b55c4cd5c1430b791a49efa7157cac.jpg')",
+        "top-image-3": "url('https://home.cern/sites/default/files/2018-06/bubble-chamber-bebc.jpg')",
+        "top-image-4": "url('https://i.stack.imgur.com/ER6mQ.jpg')",
+        "top-image-5": "url('https://www.teahub.io/photos/full/339-3394753_the-strokes-wallpaper-subatomic-particles-bubble-chamber.jpg')",
+        "top-image-6": "url('https://cds.cern.ch/record/39312/files/66954B.jpeg')"
+      },
+    },
   },
   plugins: [],
 };
